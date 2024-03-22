057c75e01f731e42e8c3f1ba67e755dc0851f356
Abhishek-Chauhan0408 <abhishek.skt02@gmail.com>
2024-03-22 14:05:09 +0530

    fix: unexpected catch removal

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://file-sharing-b.onrender.com";
-// const API_URL='https://filesharing-b.onrender.com/';
+// const API_URL='https://filesharing-b.onrender.com';
 
 export const uploadFile = async (data) => {
   try {
@@ -9,11 +9,6 @@ export const uploadFile = async (data) => {
     let response = await axios.post(`${API_URL}/upload`, data);
     console.log("response path:::", response);
     return response.data;
-   } catch (error) {
-    console.log("Error While Calling",error.message)
-   }
-}
-
   } catch (error) {
     console.log("Error While Calling", error.message);
   }
